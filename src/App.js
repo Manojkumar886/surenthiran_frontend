660b4ba14d18cf1b9790af2939a087b607bfa270
Manojkumar886 <maddymano434@gmail.com>
2023-08-24 15:27:49 +0530

    Html css bootstrap is working in react js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import OrderlistHTML, { ImgHTML, ParaHTML, TablesHTML } from "./Components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import 'bootstrap-icons/font/bootstrap-icons.css'
+
+function App()
+{
+  return(
+    <>
+    <p align='center'>Welcome to everyone</p>
+    <div align='center'>
+        <OrderlistHTML/>
+        <TablesHTML/>
+        <ParaHTML/>
+        <br/>
+        <ImgHTML/>
+        <AdvancedDesign/>
     </div>
+    <p>Thank you</p>
+    </>
+   
+  );
+}
+
+export var AdvancedDesign=()=>
+{
+  return(
+    <>
+      <div className="container text-center row justify-content-center">
+          <div className="col-4 card bg-dark">
+              <h1 className="card-title text-primary fw-bold text-uppercase">Jerry Card</h1>
+              <p className="text-danger">Card type is visiting card,credit card,debit card,atm card,invitation card</p>
+              <img className="card-img rounded-circle" src="./Tom-and-Jerry-Background.jpg"/>
+          </div>
+      </div>  
+    </>
   );
 }
 
---
+++ src/Components.js
@@ -0,0 +1,72 @@
+import './External.css'
+
+function OrderlistHTML()
+{
+    const csschange={color:'red',textShadow:'3px 4px 2px black'};
+    return(
+        <>
+        <ol>
+            Web Technolgies
+            <li style={csschange}>HTML</li>
+            <li>CSS</li>
+            <li>Bootstrap</li>
+            <li>Material UI</li>
+            <li>Bootstrap Icons</li>
+            <li>Client side Java Script</li>
+            
+        </ol>
+        </>
+    );
+}
+
+
+let TablesHTML=()=>
+{
+    return(
+        <>
+        <table>
+            <thead>
+                <tr>
+                    <th>Employee No</th>
+                    <th>Employee Name</th>
+                    <th>Employee Designation</th>
+                    <th>Employee City</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>10001</td>
+                    <td>Manojkumar</td>
+                    <td>Java Full Stack Developer</td>
+                    <td>Namakkal</td>
+                </tr>
+                <tr>
+                    <td>10002</td>
+                    <td>Surenthiran </td>
+                    <td>Junior Java Stack Developer</td>
+                    <td>Namakkal</td>
+                </tr>
+            </tbody>
+        </table>
+        </>
+    );
+}
+
+export let ParaHTML=()=>
+{
+    return(
+        <a href="./Oracle -Queries.txt" target="_blank">Oracle</a>
+    );
+}
+
+export function ImgHTML()
+{
+    return(
+        <img src="./Tom-and-Jerry-Background.jpg" width='700px' height='500px'/>
+    );
+}
+
+
+export {TablesHTML}
+
+export default OrderlistHTML;
